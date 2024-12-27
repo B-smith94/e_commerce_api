d6951ecec7534deeac69ad049645c3ab30319c67
Byron Smith <byrondsmith9@gmail.com>
2024-12-27 17:51:15 -0500

    updated requirements.txt

--- myenv/Scripts/activate.bat
+++ myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Byron\OneDrive\Desktop\Coding Materials\Mini-Projects\e_commerce_api\myenv
+set VIRTUAL_ENV=C:\Users\fenci\Desktop\Coding Materials\mini-projects\e_commerce_api\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
