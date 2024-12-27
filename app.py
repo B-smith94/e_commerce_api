bedda3e4b2e4e0b5344880d8b271f634bdacb388
Byron Smith <byrondsmith9@gmail.com>
2024-12-27 17:58:02 -0500

    fixed the app.config url

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from marshmallow import ValidationError
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Umbr3lla505@localhost/e_commerce_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Umbr3lla4850@localhost/e_commerce_db'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 CORS(app)
